fba41bd0b959dae7298bd9cc8c0e9565d5fd5487
Peter Gazdik <peter.gazdik@gmail.com>
2024-12-04 10:30:57 +0100

    Test lib

---
+++ test-gid-library/src/test/groupid/TestGroupIdClass.java
@@ -0,0 +1,11 @@
+package test.groupid;
+
+import com.braintribe.model.generic.GenericEntity;
+
+public class TestGroupIdClass {
+
+	public static void main(String[] args) {
+		System.out.println("GE TS: " + GenericEntity.class.getName());
+	}
+	
+}
